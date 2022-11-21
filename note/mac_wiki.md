35541b4ebb4b01dbbc26c6daf144a49e23a48793
yuwenjun <lineyua66@gmail.com>
2022-11-21 20:59:16 +0800

    updata

---
+++ note/mac_wiki.md
@@ -0,0 +1,8 @@
+# 截屏
+- 全屏    
+shift + command + 3
+- 部分   
+shift + command + 4
+- 将截屏内容拷贝至剪贴版    
+同时按住control
+
--- test.cpp
+++ test.cpp
@@ -1,60 +1,40 @@
 class Solution {
 private:
-    vector<int> diff;
+    vector<bool> visited;
+    // 记录一次递归堆栈中的节点
+    vector<bool> onPath;
+    bool hascycle;
 public:
-    bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>nums;
-        nums.resize(1001);
-        Difference df = new Difference(nums);
-        // 构造差分数组
-        for (int trip : trips){
-            int val = trip[0];
-            int j = trip[1];
-            int j = trip[2] - 1;
-            df.increment(i, j, val);
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph = buildgraph(numCourses, prerequisites);
+        for (int i = 0; i < numCourses; ++i){
+            traverse(graph, i);
         }
-    vector<int> res = df.result();
-    for (int i = 0; i < res.size(); ++i){
-        if (capacity < res[i]){
-            return false;
-        }
+        return !hascycle;
     }
-    return true;
-    }
-}
-// 差分数组工具类
-class Difference {
-    // 差分数组
-    private vector<int>diff;
-    
-    /* 输入一个初始数组，区间操作将在这个数组上进行 */
-    public Difference(vector<int> nums) {
-        assert nums.size() > 0;
-        diff.resize(nums.size());
-        // 根据初始数组构造差分数组
-        diff[0] = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            diff[i] = nums[i] - nums[i - 1];
+    vector<vector<int>> buildgraph(int numCourses, vector<vector<int>>& prerequisites){
+        vector<vector<int>> graph;
+        graph.resize(numCourses);
+        for (vector<int> edge : prerequisites){
+            int from = edge[1], to = edge[0];
+            graph[from].push_back(to);
         }
+        return graph;
     }
-
-    /* 给闭区间 [i, j] 增加 val（可以是负数）*/
-    public void increment(int i, int j, int val) {
-        diff[i] += val;
-        if (j + 1 < diff.size()) {
-            diff[j + 1] -= val;
+    void traverse(vector<vector<int>> graph, int s){
+        //这里为什么是onPath做判断而不是visited ？
+        // onPath会动态维护、记录当前路径，visited只会记录是否访问过；
+        if (onPath[s]){
+            hascycle = true;
         }
-    }
-
-    /* 返回结果数组 */
-    public vector<int> result() {
-        vector<int>res;
-        res.resize(diff.size())
-        // 根据差分数组构造结果数组
-        res[0] = diff[0];
-        for (int i = 1; i < diff.size(); i++) {
-            res[i] = res[i - 1] + diff[i];
+        if (visited[s] || hascycle){
+            return;
         }
-        return res;
+        visited[s] = true;
+        onPath[s] = true;
+        for (int t : graph[s]){
+            traverse(graph, t);
+        }
+        onPath[s] = false;
     }
 };
